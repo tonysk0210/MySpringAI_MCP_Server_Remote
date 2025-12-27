4000a141359fda1b1ab528c927017ad5965df036
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2025-12-27 18:39:40 +0800

    complete remote mcp server

--- src/main/java/com/example/MySpringAI_MCP_Server/MySpringAiMcpServerRemoteApplication.java
+++ src/main/java/com/example/MySpringAI_MCP_Server/MySpringAiMcpServerRemoteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MySpringAiMcpServerApplication {
+public class MySpringAiMcpServerRemoteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringAiMcpServerApplication.class, args);
+		SpringApplication.run(MySpringAiMcpServerRemoteApplication.class, args);
 	}
 
 }
